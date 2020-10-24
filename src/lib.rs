8d26daa038ec3172c9be3b3328f25a3a33eeea45
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-10-24 12:47:39 +0200

    Add usage section to docs

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,20 @@
 //! Change.rs provides an easy-to-use CLI (command line interface) to calculate
 //! change directly from the command line. Supports ouputing to `stdout` for
 //! results and `stderr` for possible errors.
+//! 
+//! ## Usage
+//! 
+//! ```bash
+//! changers [amount] [optional: coin_file]
+//! ```
+//! 
+//! The change\.rs CLI accepts two arguments to calculate change:
+//! 
+//! -   `amount`: The monetary amount to make change for
+//! -   `coin_file`, optional: An optional coin file (see [examples](examples/)
+//!     folder) containing a list of coins seperated by a newline. If no file is
+//!     passed, coins are read from stdin.
+//!
 
 pub mod cli;
 pub mod logic;
